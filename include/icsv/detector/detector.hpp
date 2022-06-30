16f7c1cb827d8dcd8a6fd0628cd5d0b539f6b548
V.Vakras <vasilis_98@hotmail.com>
2022-06-30 20:12:24 +0300

    Added DetectorReport struct, SniffCode returns a Report

--- include/icsv/detector/detector.hpp
+++ include/icsv/detector/detector.hpp
@@ -2,14 +2,23 @@
 
 #include <string>
 
+// TODO: Add interface documentation
 namespace detector {
+
+struct DetectorReport {
+  std::string message;
+  std::string source;
+  int         level;
+};
+
+using Report = DetectorReport;
 
 class Detector {
 public:
-  Detector()          = default;
+  Detector();
   virtual ~Detector() = default;
 
-  virtual int SniffCode(const std::string&) = 0;
+  virtual auto SniffCode(const std::string&) -> Report = 0;
 };
 
 }  // namespace detector
