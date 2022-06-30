e9504369acd1fae59f27691ac70453ab9db719ab
V.Vakras <vasilis_98@hotmail.com>
2022-06-30 20:11:22 +0300

    Added Register method, and private Detector list

--- include/icsv/detector/detector_manager.hpp
+++ include/icsv/detector/detector_manager.hpp
@@ -1,16 +1,24 @@
 #pragma once
 
+#include <list>
+
 namespace detector {
+
+class Detector;
 
 class DetectorManager final {
 public:
   static auto Get() -> DetectorManager&;
+
+  void RegisterDetector(Detector*);
 
 private:
   DetectorManager()                       = default;
   DetectorManager(const DetectorManager&) = delete;
   DetectorManager(DetectorManager&&)      = delete;
   ~DetectorManager();
+
+  std::list<Detector*> m_detectors;
 };
 
 }  // namespace detector
