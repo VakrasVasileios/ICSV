b822d4f74cb94d965a27e8e904fcebeaab853a87
V.Vakras <vasilis_98@hotmail.com>
2022-06-30 20:10:31 +0300

    On destruction all registered Detectors are deleted. Implented Register method.

--- src/detector/detector_manager.cpp
+++ src/detector/detector_manager.cpp
@@ -1,13 +1,22 @@
 #include "detector/detector_manager.hpp"
+#include "detector/detector.hpp"
 
 namespace detector {
 
-DetectorManager::~DetectorManager() {}
+DetectorManager::~DetectorManager() {
+  for (auto* det : m_detectors)
+    delete det;
+}
 
 auto
 DetectorManager::Get() -> DetectorManager& {
   static DetectorManager singleton;
   return singleton;
+}
+
+void
+DetectorManager::RegisterDetector(Detector* det) {
+  m_detectors.push_back(det);
 }
 
 }  // namespace detector
