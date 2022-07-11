e3ff9fe8c599c214c70734417e3b2f053d280b31
V.Vakras <vasilis_98@hotmail.com>
2022-07-11 20:34:39 +0300

    added build file to be ignored. Fixed Cmake files. Deleted detector.cpp

--- src/detector/detector.cpp
+++
@@ -1,8 +0,0 @@
-#include "detector/detector.hpp"
-#include "detector/detector_manager.hpp"
-
-namespace detector {
-
-Detector::Detector() { DetectorManager::Get().RegisterDetector(this); }
-
-}  // namespace detector
