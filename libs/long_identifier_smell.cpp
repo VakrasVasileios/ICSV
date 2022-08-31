01f079cba7f5acdc3f57870c79aa6215fae32af5
V.Vakras <vasilis_98@hotmail.com>
2022-08-31 18:19:28 +0300

    Removed GetCleanIdentifier since json graph already has it

--- libs/long_identifier_smell.cpp
+++ libs/long_identifier_smell.cpp
@@ -8,28 +8,9 @@ public:
   ~LongIdentifierDet() override = default;
 
   void SmellDetect(const Json::Value& arch);
-
-private:
-  auto GetCleanIdentifier(const std::string& id) -> std::string;
 };
 
 LongIdentifierDet* l_id = new LongIdentifierDet();
-
-auto
-LongIdentifierDet::GetCleanIdentifier(const std::string& id)
-    -> std::string {  // FIXME: using the updated graphs
-  assert(!id.empty());
-  std::size_t tmp;
-  auto        namespace_end
-      = (tmp = id.find_last_of(':')) == std::string::npos ? 0 : tmp + 1;
-  auto        args_start = (tmp = id.find_first_of('(')) == std::string::npos
-             ? id.size() - 1
-             : tmp - 1;
-  std::string identifier
-      = id.substr(namespace_end, args_start - namespace_end + 1);
-
-  return identifier;
-}
 
 void
 LongIdentifierDet::SmellDetect(const Json::Value& arch) {
@@ -37,15 +18,15 @@ LongIdentifierDet::SmellDetect(const Json::Value& arch) {
        strct != arch["structures"].end();
        strct++) {
 
-    auto           id_size = GetCleanIdentifier(strct.name()).size();
+    auto           id_size = (*strct)["name"].asString().size();
     DetectorReport strct_rep;
-    strct_rep.message = "Structure \"" + strct.name() + "\" has an id of "
-        + std::to_string(id_size) + " characters";
+    strct_rep.message = "Structure \"" + (*strct)["name"].asString()
+        + "\" has an id of " + std::to_string(id_size) + " characters";
     strct_rep.level    = id_size;
     strct_rep.src_info = SourceInfo((*strct)["src_info"]["file"].asString(),
                                     (*strct)["src_info"]["line"].asInt(),
                                     (*strct)["src_info"]["col"].asInt(),
-                                    strct.name());
+                                    (*strct)["name"].asString());
     REGISTER_REPORT("long_identifier", strct_rep);
     std::cout << strct_rep << std::endl;
 
@@ -53,16 +34,16 @@ LongIdentifierDet::SmellDetect(const Json::Value& arch) {
          meth != (*strct)["methods"].end();
          meth++) {
 
-      id_size = GetCleanIdentifier(meth.name()).size();
+      id_size = (*meth)["name"].asString().size();
       DetectorReport meth_rep;
-      meth_rep.message = "Method \"" + meth.name() + "\" has an id of "
-          + std::to_string(id_size) + " characters";
+      meth_rep.message = "Method \"" + (*meth)["name"].asString()
+          + "\" has an id of " + std::to_string(id_size) + " characters";
       meth_rep.level    = id_size;
       meth_rep.src_info = SourceInfo((*meth)["src_info"]["file"].asString(),
                                      (*meth)["src_info"]["line"].asInt(),
                                      (*meth)["src_info"]["col"].asInt(),
-                                     strct.name(),
-                                     meth.name());
+                                     (*strct)["name"].asString(),
+                                     (*meth)["name"].asString());
       REGISTER_REPORT("long_identifier", meth_rep);
 
       std::cout << meth_rep << std::endl;
@@ -71,17 +52,17 @@ LongIdentifierDet::SmellDetect(const Json::Value& arch) {
            arg != (*strct)["methods"]["args"].end();
            arg++) {
 
-        id_size = GetCleanIdentifier(arg.name()).size();
+        id_size = (*arg)["name"].asString().size();
         DetectorReport arg_rep;
-        arg_rep.message = "Argument \"" + arg.name() + "\" of method \""
-            + meth.name() + "\" has an id of " + std::to_string(id_size)
-            + " characters";
+        arg_rep.message = "Argument \"" + (*arg)["name"].asString()
+            + "\" of method \"" + (*meth)["name"].asString()
+            + "\" has an id of " + std::to_string(id_size) + " characters";
         arg_rep.level    = id_size;
         arg_rep.src_info = SourceInfo((*arg)["src_info"]["file"].asString(),
                                       (*arg)["src_info"]["line"].asInt(),
                                       (*arg)["src_info"]["col"].asInt(),
-                                      strct.name(),
-                                      meth.name());
+                                      (*strct)["name"].asString(),
+                                      (*meth)["name"].asString());
         REGISTER_REPORT("long_identifier", arg_rep);
 
         std::cout << arg_rep << std::endl;
@@ -91,17 +72,17 @@ LongIdentifierDet::SmellDetect(const Json::Value& arch) {
            def != (*strct)["methods"]["definitions"].end();
            def++) {
 
-        id_size = GetCleanIdentifier(def.name()).size();
+        id_size = (*def)["name"].asString().size();
         DetectorReport def_rep;
-        def_rep.message = "Definition \"" + def.name() + "\" in method \""
-            + meth.name() + "\" has an id of " + std::to_string(id_size)
-            + " characters";
+        def_rep.message = "Definition \"" + (*def)["name"].asString()
+            + "\" in method \"" + (*meth)["name"].asString()
+            + "\" has an id of " + std::to_string(id_size) + " characters";
         def_rep.level    = id_size;
         def_rep.src_info = SourceInfo((*def)["src_info"]["file"].asString(),
                                       (*def)["src_info"]["line"].asInt(),
                                       (*def)["src_info"]["col"].asInt(),
-                                      strct.name(),
-                                      meth.name());
+                                      (*strct)["name"].asString(),
+                                      (*meth)["name"].asString());
         REGISTER_REPORT("long_identifier", def_rep);
         std::cout << def_rep << std::endl;
       }
@@ -110,16 +91,16 @@ LongIdentifierDet::SmellDetect(const Json::Value& arch) {
          field != (*strct)["fields"].end();
          field++) {
 
-      id_size = GetCleanIdentifier(field.name()).size();
+      id_size = (*field)["name"].asString().size();
       DetectorReport fld_rep;
-      fld_rep.message = "Field \"" + field.name() + "\" of structure \""
-          + strct.name() + "\" has an id of " + std::to_string(id_size)
-          + " characters";
+      fld_rep.message = "Field \"" + (*field)["name"].asString()
+          + "\" of structure \"" + (*strct)["name"].asString()
+          + "\" has an id of " + std::to_string(id_size) + " characters";
       fld_rep.level    = id_size;
       fld_rep.src_info = SourceInfo((*field)["src_info"]["file"].asString(),
                                     (*field)["src_info"]["line"].asInt(),
                                     (*field)["src_info"]["col"].asInt(),
-                                    strct.name());
+                                    (*strct)["name"].asString());
       REGISTER_REPORT("long_identifier", fld_rep);
       std::cout << fld_rep << std::endl;
     }
