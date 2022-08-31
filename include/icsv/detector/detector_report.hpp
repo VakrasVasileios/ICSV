80934c7635cae5b85e0c3e30197ad0c4c9e00e7a
V.Vakras <vasilis_98@hotmail.com>
2022-08-31 15:10:09 +0300

    FIxed cout overload of Report struct

--- include/icsv/detector/detector_report.hpp
+++ include/icsv/detector/detector_report.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <string>
 
 namespace icsv::detector {
@@ -9,5 +10,13 @@ typedef struct DetectorReport {
   std::string source  = "";
   int         level   = -1;
 } Report;
+
+inline auto
+operator<<(std::ostream& os, const icsv::detector::Report& rep)
+    -> std::ostream& {
+  return os << "Message: " << rep.message << '\n'
+            << "Source: " << rep.source << '\n'
+            << "Level: " << rep.level << '\n';
+}
 
 }  // namespace icsv::detector
