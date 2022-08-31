96527112be19b5e691dffbbf3c3663edcb88c1cb
V.Vakras <vasilis_98@hotmail.com>
2022-08-31 12:48:03 +0300

    changed large file detector to inherit Detector class

--- libs/large_file_smell.cpp
+++ libs/large_file_smell.cpp
@@ -13,12 +13,25 @@ file_line_count(const std::string& path) {
   return fcount;
 }
 
-auto large_file = DETECTOR {
+class LargeFileDetector : public icsv::detector::Detector {
+public:
+  LargeFileDetector() : icsv::detector::Detector("large_file_det") {}
+  ~LargeFileDetector() override = default;
+
+  void SmellDetect(const Json::Value& arch);
+};
+
+LargeFileDetector lgd;
+
+void
+LargeFileDetector::SmellDetect(const Json::Value& arch) {
   for (auto& src : arch["sources"]) {
     DetectorReport rep;
-    unsigned       lines = file_line_count(src.asString());  // FIXME
-    rep.level            = lines;
-    rep.message          = "Source file: " + src.asString() + " has "
+    unsigned       lines
+        = file_line_count(src.asString());  // FIXME: This will need the full,
+                                            // json has only file name
+    rep.level   = lines;
+    rep.message = "Source file: " + src.asString() + " has "
         + std::to_string(lines) + " of code.";
     rep.source = src.asString();
     REGISTER_REPORT("large_file", rep);
