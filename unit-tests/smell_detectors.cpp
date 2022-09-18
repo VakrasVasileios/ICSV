222007171eae63501848d0aa75ce209bba2c4927
V.Vakras <vasilis_98@hotmail.com>
2022-09-18 13:00:23 +0300

    Renamed unit test file for smell detectors

--- unit-tests/smell_detectors.cpp
+++ unit-tests/smell_detectors.cpp
@@ -10,13 +10,14 @@ namespace icsv::unit_tests {
 
 class Correct_level_assignment : public testing::Test {
   void SetUp() override {
-    detector::arch::ArchitectureHolder::Get().DeserializeArchitecture(
+    icsv::detector::arch::ArchitectureHolder::Get().DeserializeArchitecture(
         "/home/vkrs/Documents/ICSV/unit-tests/graph.json");
-    detector::EvaluationCenter::Get().DeseriallizeConfig(
+    icsv::detector::EvaluationCenter::Get().DeseriallizeConfig(
         "/home/vkrs/Documents/ICSV/unit-tests/DetectorsConfig.json");
     icsv::detector::DetectorManager::Get().UseDetectors();
   }
 };
+
 TEST_F(Correct_level_assignment,
        Checking_correct_scoring_for_large_file_smell) {
   auto& reps
@@ -30,6 +31,25 @@ TEST_F(Correct_level_assignment,
 TEST_F(Correct_level_assignment, Checking_correct_scoring_for_long_id_smell) {
   auto& reps = icsv::detector::ReportCenter::Get().GetReportsByTag(
       "Excessively long identifier");
+  for (auto& r : reps) {
+    GTEST_ASSERT_GE(r.level, 0);
+    GTEST_ASSERT_LE(r.level, 10);
+  }
+}
+#if (0)
+TEST_F(Correct_level_assignment, Checking_correct_scoring_for_nested_scopes) {
+  auto& reps
+      = icsv::detector::ReportCenter::Get().GetReportsByTag("Nested Scopes");
+  for (auto& r : reps) {
+    GTEST_ASSERT_GE(r.level, 0);
+    GTEST_ASSERT_LE(r.level, 10);
+  }
+}
+#endif
+TEST_F(Correct_level_assignment,
+       Checking_correct_scoring_for_dependency_count) {
+  auto& reps = icsv::detector::ReportCenter::Get().GetReportsByTag(
+      "Many Dependencies");
   for (auto& r : reps) {
     GTEST_ASSERT_GE(r.level, 0);
     GTEST_ASSERT_LE(r.level, 10);
