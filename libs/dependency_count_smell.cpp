888a5a52c9df4c9ea427dfef9a8d86f0cb54c4ad
V.Vakras <vasilis_98@hotmail.com>
2022-09-18 13:01:46 +0300

    Implemented dependency count and nested scopes smell detectors

---
+++ libs/dependency_count_smell.cpp
@@ -0,0 +1,54 @@
+#include "range_based_smell_eval.hpp"
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#define TAG "Many Dependencies"
+#include "icsv/detector/detector.hpp"
+
+class DependencyCountDet : Detector {
+public:
+  DependencyCountDet() : Detector(TAG) {}
+  ~DependencyCountDet() override = default;
+
+  void DetectSmell(const ArchData& arch) override;
+};
+
+static DependencyCountDet* dcd = new DependencyCountDet();
+CREATE_RANGE_BASED_EVAL(TAG);
+
+void
+DependencyCountDet::DetectSmell(const ArchData& arch) {
+  using DepMap = std::unordered_map<std::string, std::vector<std::string>>;
+  DepMap dep_map;
+
+  for (auto& dep : arch.dependencies)
+    dep_map[dep.from].emplace_back(dep.to);
+
+  for (auto& dep_str : dep_map) {
+    DetectorReport rep;
+
+    rep.level             = EVAL(dep_str.second.size());
+    std::string dep_names = "";
+    for (std::size_t i = 0; i < dep_str.second.size() - 1; i++)
+      dep_names += dep_str.second[i] + ", ";
+    dep_names += dep_str.second[dep_str.second.size() - 1] + '\n';
+
+    auto strct  = std::find_if(arch.structures.begin(),
+                              arch.structures.end(),
+                              [dep_str](const auto& m) {
+                                if (m.signature == dep_str.first)
+                                  return true;
+                                else
+                                  return false;
+                              });
+    rep.message = "Structure \"" + dep_str.first + "\" directly depends upon "
+        + std::to_string(dep_str.second.size())
+        + " other classes. These are: " + dep_names;
+    rep.src_info = SourceInfo(strct->src_info.file,
+                              strct->src_info.line,
+                              strct->src_info.col,
+                              dep_str.first);
+    REPORT(rep);
+  }
+}
---
+++ libs/nested_scopes_smell.cpp
@@ -0,0 +1,34 @@
+#include "range_based_smell_eval.hpp"
+#include <assert.h>
+
+#define TAG "Nested Scopes"
+#include "icsv/detector/detector.hpp"
+
+class NestedScopesDet : Detector {
+public:
+  NestedScopesDet() : Detector(TAG) {}
+  ~NestedScopesDet() override = default;
+
+  void DetectSmell(const ArchData& arch) override;
+};
+
+static NestedScopesDet* nsd = new NestedScopesDet();
+CREATE_RANGE_BASED_EVAL(TAG);
+
+void
+NestedScopesDet::DetectSmell(const ArchData& arch) {
+  for (auto& strct : arch.structures) {
+    for (auto& meth : strct.methods) {
+      DetectorReport rep;
+      rep.level   = EVAL(meth.max_scope);
+      rep.message = "Method \"" + meth.signature + "\" has "
+          + std::to_string(rep.level) + " nested scopes.";
+      rep.src_info = SourceInfo(meth.src_info.file,
+                                meth.src_info.line,
+                                meth.src_info.col,
+                                strct.signature,
+                                meth.signature);
+      REPORT(rep);
+    }
+  }
+}
