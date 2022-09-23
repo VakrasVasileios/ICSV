2b63b93cbcedd6cb161a429304af270b99d5d51e
V.Vakras <vasilis_98@hotmail.com>
2022-09-23 21:16:38 +0300

    Added a macro for the tests, that uses a try/catch block for the exception

--- unit-tests/smell_detectors.cpp
+++ unit-tests/smell_detectors.cpp
@@ -6,6 +6,18 @@
 #include "gtest/gtest.h"
 #include <iostream>
 
+#define ASSERT_REPORTS(tag)                                                \
+  try {                                                                    \
+    auto& reps = icsv::detector::ReportCenter::Get().GetReportsByTag(tag); \
+    for (auto& r : reps) {                                                 \
+      GTEST_ASSERT_GE(r.level, 0);                                         \
+      GTEST_ASSERT_LE(r.level, 10);                                        \
+    }                                                                      \
+  } catch (icsv::detector::NoRegisteredReports & e) {                      \
+    std::cout << e.what() << std::endl;                                    \
+    GTEST_ASSERT_TRUE(true);                                               \
+  }
+
 namespace icsv::unit_tests {
 
 class Correct_level_assignment : public testing::Test {
@@ -20,40 +32,44 @@ class Correct_level_assignment : public testing::Test {
 
 TEST_F(Correct_level_assignment,
        Checking_correct_scoring_for_large_file_smell) {
-  auto& reps
-      = icsv::detector::ReportCenter::Get().GetReportsByTag("Large File");
-  for (auto& r : reps) {
-    GTEST_ASSERT_GE(r.level, 0);
-    GTEST_ASSERT_LE(r.level, 10);
-  }
+  ASSERT_REPORTS("Large File");
 }
 
 TEST_F(Correct_level_assignment, Checking_correct_scoring_for_long_id_smell) {
-  auto& reps = icsv::detector::ReportCenter::Get().GetReportsByTag(
-      "Excessively long identifier");
-  for (auto& r : reps) {
-    GTEST_ASSERT_GE(r.level, 0);
-    GTEST_ASSERT_LE(r.level, 10);
-  }
+  ASSERT_REPORTS("Excessively long identifier");
 }
-#if (0)
 TEST_F(Correct_level_assignment, Checking_correct_scoring_for_nested_scopes) {
-  auto& reps
-      = icsv::detector::ReportCenter::Get().GetReportsByTag("Nested Scopes");
-  for (auto& r : reps) {
-    GTEST_ASSERT_GE(r.level, 0);
-    GTEST_ASSERT_LE(r.level, 10);
-  }
+  ASSERT_REPORTS("Nested Scopes");
 }
-#endif
+
 TEST_F(Correct_level_assignment,
        Checking_correct_scoring_for_dependency_count) {
-  auto& reps = icsv::detector::ReportCenter::Get().GetReportsByTag(
-      "Many Dependencies");
-  for (auto& r : reps) {
-    GTEST_ASSERT_GE(r.level, 0);
-    GTEST_ASSERT_LE(r.level, 10);
-  }
+  ASSERT_REPORTS("Many Dependencies");
+}
+
+TEST_F(Correct_level_assignment,
+       Checking_correct_scoring_for_function_overload_count) {
+  ASSERT_REPORTS("Excessive function overloading");
+}
+
+TEST_F(Correct_level_assignment,
+       Checking_correct_scoring_for_non_virtual_base_destructor) {
+  ASSERT_REPORTS("Non virtual base destructor");
+}
+
+TEST_F(Correct_level_assignment,
+       Checking_correct_scoring_for_method_argument_count) {
+  ASSERT_REPORTS("Too many parameters");
+}
+
+TEST_F(Correct_level_assignment,
+       Checking_correct_scoring_for_method_literal_count) {
+  ASSERT_REPORTS("Too many function literals");
+}
+
+TEST_F(Correct_level_assignment,
+       Checking_correct_scoring_for_method_definition_count) {
+  ASSERT_REPORTS("Too many locals");
 }
 
 }  // namespace icsv::unit_tests
