3794683c00aba48add8335af56f03174e712a2cc
V.Vakras <vasilis_98@hotmail.com>
2022-10-18 14:01:07 +0300

    Implemented redundant protected and naming convention detector

---
+++ libs/detector_impls/naming_conventions_det.cpp
@@ -0,0 +1,93 @@
+#include "../evaluator_impls/regex_based_eval.hpp"
+#include "utils/utils.hpp"
+
+#define TAG "Naming conventions"
+#include "icsv/detector/detector.hpp"
+
+class NamingConventionsDet : public Detector {
+public:
+  NamingConventionsDet() : Detector(TAG) {}
+  ~NamingConventionsDet() override = default;
+
+  void DetectSmell(const ArchData& arch) override;
+};
+
+static NamingConventionsDet* ncd = new NamingConventionsDet();
+CREATE_REGEX_BASED_EVAL(TAG);
+
+void
+NamingConventionsDet::DetectSmell(const ArchData& arch) {
+  for (auto& strct : arch.structures) {
+    DetectorReport strct_rep;
+    strct_rep.init_level = eval->EvaluateClassName(strct.name);
+    strct_rep.level      = EVAL(strct_rep.init_level);
+    strct_rep.message    = "Structure \"" + strct.signature
+        + "\" has an id deviating from standard naming convention by "
+        + std::to_string(strct_rep.init_level) + " characters.";
+    strct_rep.src_info = SourceInfo(strct.src_info.file,
+                                    strct.src_info.line,
+                                    strct.src_info.col,
+                                    strct.signature);
+    REPORT(strct_rep);
+
+    for (auto& meth : strct.methods) {
+      if (!icsv::utils::is_standard_class_func(meth.name, strct.name)) {
+        DetectorReport meth_rep;
+        meth_rep.init_level = eval->EvaluateMethodName(meth.name);
+        meth_rep.level      = EVAL(meth_rep.init_level);
+        meth_rep.message    = "Method \"" + meth.name
+            + "\" has an id deviating from standard naming convention by "
+            + std::to_string(meth_rep.init_level) + " characters.";
+        meth_rep.src_info = SourceInfo(meth.src_info.file,
+                                       meth.src_info.line,
+                                       meth.src_info.col,
+                                       strct.signature,
+                                       meth.signature);
+        REPORT(meth_rep);
+      }
+
+      for (auto& arg : meth.args) {
+        DetectorReport arg_rep;
+        arg_rep.init_level = eval->EvaluateVarName(arg.name);
+        arg_rep.level      = EVAL(arg_rep.init_level);
+        arg_rep.message    = "Argument \"" + arg.name
+            + " has an id deviating from standard naming conventions by "
+            + std::to_string(arg_rep.init_level) + " characters.";
+        arg_rep.src_info = SourceInfo(meth.src_info.file,
+                                      meth.src_info.line,
+                                      meth.src_info.col,
+                                      strct.signature,
+                                      meth.signature);
+      }
+
+      for (auto& def : meth.definitions) {
+        DetectorReport def_rep;
+        def_rep.init_level = eval->EvaluateVarName(def.name);
+        def_rep.level      = EVAL(def_rep.init_level);
+        def_rep.message    = "Definition \"" + def.name
+            + "\" has an id deviating from standard naming convention by "
+            + std::to_string(def_rep.init_level) + " characters.";
+        def_rep.src_info = SourceInfo(def.src_info.file,
+                                      def.src_info.line,
+                                      def.src_info.col,
+                                      strct.signature,
+                                      meth.signature);
+        REPORT(def_rep);
+      }
+    }
+
+    for (auto& fld : strct.fields) {
+      DetectorReport fld_rep;
+      fld_rep.init_level = eval->EvaluateVarName(fld.definition.name);
+      fld_rep.level      = EVAL(fld_rep.init_level);
+      fld_rep.message    = "Field \"" + fld.signature
+          + "\" has an id deviating from standard naming convention by "
+          + std::to_string(fld_rep.init_level) + " characters.";
+      fld_rep.src_info = SourceInfo(fld.definition.src_info.file,
+                                    fld.definition.src_info.line,
+                                    fld.definition.src_info.col,
+                                    strct.signature);
+      REPORT(fld_rep);
+    }
+  }
+}
---
+++ libs/detector_impls/redundant_protected_det.cpp
@@ -0,0 +1,55 @@
+#include "../evaluator_impls/range_based_eval.hpp"
+#include "utils/utils.hpp"
+#include <assert.h>
+
+#define TAG "Redundant protected access"
+#include "icsv/detector/detector.hpp"
+
+class RedundantProtectedDet : public Detector {
+public:
+  RedundantProtectedDet() : Detector(TAG) {}
+  ~RedundantProtectedDet() override = default;
+
+  void DetectSmell(const ArchData& arch) override;
+};
+
+static RedundantProtectedDet* rpd = new RedundantProtectedDet();
+CREATE_RANGE_BASED_EVAL(TAG);
+
+void
+RedundantProtectedDet::DetectSmell(const ArchData& arch) {
+  icsv::utils::BasesList blist = icsv::utils::get_base_structures(arch);
+
+  for (auto strct : arch.structures) {
+    if (std::find_if(blist.begin(),
+                     blist.end(),
+                     [&strct](const auto& b) {
+                       return strct.signature == b ? true : false;
+                     })
+        == blist.end())
+      continue;
+
+    int protected_members = 0;
+    for (auto meth : strct.methods) {
+      if (meth.access == "protected")
+        protected_members++;
+    }
+    for (auto field : strct.fields) {
+      if (field.access == "protected")
+        protected_members++;
+    }
+
+    DetectorReport rep;
+    rep.init_level = protected_members;
+    rep.level      = EVAL(protected_members);
+    rep.message    = "Class \"" + strct.signature
+        + "\" is not inherited from and has "
+        + std::to_string(protected_members)
+        + " protected variables or methods.";
+    rep.src_info = SourceInfo(strct.src_info.file,
+                              strct.src_info.line,
+                              strct.src_info.col,
+                              strct.signature);
+    REPORT(rep);
+  }
+}
