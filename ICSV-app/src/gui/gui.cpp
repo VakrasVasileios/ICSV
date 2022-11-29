cf65b01eaf48c6620b862740c408575d45ae134b
V.Vakras <vasilis_98@hotmail.com>
2022-11-29 12:48:45 +0200

    Added report reference along with a setter and show report private method

--- ICSV-app/src/gui/gui.cpp
+++ ICSV-app/src/gui/gui.cpp
@@ -2,6 +2,8 @@
 #include "icsv/detector/arch/architecture_holder.hpp"
 #include "icsv/detector/detector_manager.hpp"
 #include "icsv/detector/evaluation_center.hpp"
+#include "icsv/detector/report_center.hpp"
+#include "../entity_manager.hpp"
 #include "iostream"
 
 namespace ICSVapp {
@@ -10,10 +12,10 @@ void
 IcsvGui::Display(void) {
   ImGui::Begin("Menu");
   ShowConfigSelect();
+  ShowSmellButton();
   ImGui::Separator();
   ImGui::Text("Smell Report");
-  // check if something was clicked by Raycast
-  // and Display Report
+  ShowDetectorReport();
   ImGui::End();
 }
 
@@ -51,6 +53,56 @@ IcsvGui::ShowConfigSelect(void) {
   }
   m_conf_brwsr.Display();
   m_graph_brwsr.Display();
+}
+
+void
+IcsvGui::ShowDetectorReport(void) {
+  if (m_rep_dspld != nullptr) {
+    ImGui::Begin("Detector Report");
+    ImGui::BeginMenu("");
+    ImGui::MenuItem("Message: ");
+    ImGui::SameLine();
+    ImGui::MenuItem(m_rep_dspld->message.c_str());
+    ImGui::Separator();
+
+    ImGui::MenuItem("Level: ");
+    ImGui::SameLine();
+    ImGui::MenuItem(std::to_string(m_rep_dspld->level).c_str());
+    ImGui::Separator();
+
+    ImGui::MenuItem("File: ");
+    ImGui::SameLine();
+    ImGui::MenuItem(m_rep_dspld->src_info.file.c_str());
+    ImGui::Separator();
+
+    ImGui::MenuItem("Line: ");
+    ImGui::SameLine();
+    ImGui::MenuItem(std::to_string(m_rep_dspld->src_info.line).c_str());
+    ImGui::Separator();
+
+    ImGui::MenuItem("Column: ");
+    ImGui::SameLine();
+    ImGui::MenuItem(std::to_string(m_rep_dspld->src_info.col).c_str());
+    ImGui::Separator();
+
+    ImGui::MenuItem("Structure: ");
+    ImGui::SameLine();
+    ImGui::MenuItem(m_rep_dspld->src_info.strct.c_str());
+    ImGui::Separator();
+
+    ImGui::MenuItem("Method: ");
+    ImGui::SameLine();
+    ImGui::MenuItem(m_rep_dspld->src_info.method.c_str());
+  }
+  ImGui::End();
+}
+
+void
+IcsvGui::ShowSmellButton(void) {
+  if (ImGui::Button("Smell Code")) {
+    icsv::detector::use_smell_detectors();
+    // TODO: create icsv entities
+  }
 }
 
 }  // namespace ICSVapp
--- ICSV-app/src/gui/gui.hpp
+++ ICSV-app/src/gui/gui.hpp
@@ -2,8 +2,11 @@
 
 #include <imgui/imgui.h>
 #include "imguifilebrowser.hpp"
+#include "icsv/detector/detector_report.hpp"
 
 namespace ICSVapp {
+
+using DetectorReport = icsv::detector::DetectorReport;
 
 class IcsvGui final {
 public:
@@ -11,12 +14,16 @@ public:
   ~IcsvGui() = default;
 
   void Display(void);
+  void SetReportToDisplay(DetectorReport* rep) { m_rep_dspld = rep; }
 
 private:
-  ImGui::FileBrowser m_graph_brwsr;
-  ImGui::FileBrowser m_conf_brwsr;
+  ImGui::FileBrowser              m_graph_brwsr;
+  ImGui::FileBrowser              m_conf_brwsr;
+  icsv::detector::DetectorReport* m_rep_dspld{ nullptr };
 
   void ShowConfigSelect(void);
+  void ShowDetectorReport(void);
+  void ShowSmellButton(void);
 };
 
 }  // namespace ICSVapp
