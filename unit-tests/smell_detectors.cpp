877a754977b8fc30a5576f4a4df72ea456d3b216
V.Vakras <vasilis_98@hotmail.com>
2023-10-31 22:34:57 +0200

    added some tests

--- unit-tests/smell_detectors.cpp
+++ unit-tests/smell_detectors.cpp
@@ -5,6 +5,8 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include <iostream>
+#include <string>
+#include <tuple>
 
 #define ASSERT_REPORTS(tag)                                               \
   try {                                                                   \
@@ -22,13 +24,80 @@ namespace icsv::unit_tests {
 
 class Correct_level_assignment : public testing::Test {
   void SetUp() override {
-    icsv::detector::arch::ArchitectureHolder::Get().DeserializeArchitecture(
-        "/home/vkrs/Documents/ICSV/unit-tests/graphme2.json");
-    icsv::detector::EvaluationCenter::Get().DeseriallizeConfig(
-        "/home/vkrs/Documents/ICSV/unit-tests/DetectorsConfig.json");
-    icsv::detector::DetectorManager::Get().UseDetectors();
+    static bool init = false;
+    if (!init) {
+      icsv::detector::arch::ArchitectureHolder::Get().DeserializeArchitecture(
+          "/home/vkrs/Documents/ICSV/unit-tests/graphme2.json");
+      icsv::detector::EvaluationCenter::Get().DeseriallizeConfig(
+          "/home/vkrs/Documents/ICSV/unit-tests/DetectorsConfig.json");
+      icsv::detector::DetectorManager::Get().UseDetectors();
+      init = true;
+    }
   }
 };
+
+class Always_Greater_Equal_to_Zero : public testing::Test {};
+
+class String_Literal_Template : public testing::Test {};
+
+void
+always_GE_zero(int& num) {
+  num = num * (num >= 0);
+}
+
+enum MType { RANGE, REGEX, BOOL };
+
+template <MType T, typename... Targs>
+auto
+some(Targs... args) -> std::tuple<char, Targs...> {
+  return { '0', args... };
+}
+
+template <>
+auto
+some<RANGE, std::string, int>(std::string s, int i)
+    -> std::tuple<char, std::string, int> {
+  return { 'R', s, i };
+}
+
+template <>
+auto
+some<REGEX, std::string, std::string>(std::string s1, std::string s2)
+    -> std::tuple<char, std::string, std::string> {
+  return { 'X', s1, s2 };
+}
+
+template <>
+auto
+some<BOOL, std::string, bool>(std::string s, bool b)
+    -> std::tuple<char, std::string, bool> {
+  return { 'B', s, b };
+}
+
+TEST_F(String_Literal_Template, Checking_string_literal_template) {
+  GTEST_ASSERT_TRUE(some<RANGE>(std::string("tag"), 10)
+                    == std::make_tuple('R', std::string("tag"), 10));
+  GTEST_ASSERT_TRUE(
+      some<REGEX>(std::string("tag"), std::string("regex"))
+      == std::make_tuple('X', std::string("tag"), std::string("regex")));
+  GTEST_ASSERT_TRUE(some<BOOL>(std::string("tag"), true)
+                    == std::make_tuple('B', std::string("tag"), true));
+}
+
+TEST_F(Always_Greater_Equal_to_Zero, Checking_always_greater_equal_to_zero) {
+  int num = 10;
+  always_GE_zero(num);
+  GTEST_ASSERT_TRUE(num >= 0);
+  num = 30022;
+  always_GE_zero(num);
+  GTEST_ASSERT_TRUE(num >= 0);
+  num = -1;
+  always_GE_zero(num);
+  GTEST_ASSERT_TRUE(num == 0);
+  num = -109743;
+  always_GE_zero(num);
+  GTEST_ASSERT_TRUE(num == 0);
+}
 
 TEST_F(Correct_level_assignment,
        Checking_correct_scoring_for_large_file_smell) {
